e076a713262789f2c32efb84b901c07c75b62902
Oni Harnantyo <oni.harnantyo97@gmail.com>
2022-12-29 16:06:57 +0700

    feat: added parallax on home donate (#16)
    Co-authored-by: oni_harnantyo <oni_harnantyo@linkaja.io>

--- src/components/Organisms/HomeDonate/HomeDonate.tsx
+++ src/components/Organisms/HomeDonate/HomeDonate.tsx
@@ -5,6 +5,7 @@ import DonateBannerImage from '@assets/image/donate-banner.webp';
 import { StyledButton } from '@components/Atoms/StyledButton';
 import clsx from 'clsx';
 import { Col, Row } from 'react-bootstrap';
+import { Parallax } from 'react-parallax';
 
 import { SectionNoTitle } from '../Section';
 import Style from './HomeDonate.module.scss';
@@ -12,36 +13,39 @@ import Style from './HomeDonate.module.scss';
 const HomeDonate = () => {
   return (
     <div className={clsx(Style.donate, 'd-flex')}>
-      <div className={Style.backgroundImageWrapper}>
-        <Image
-          src={DonateBannerImage}
-          layout="fill"
-          objectFit="cover"
-          alt="vidyasena donate background"
-        />
-      </div>
-      <div className="align-self-center mx-auto">
-        <SectionNoTitle>
-          <Row className={Style.content}>
-            <Col md={6} className="mx-md-auto mx-lg-auto my-md-auto my-lg-auto">
-              <h2 className={clsx(Style.title, 'text-md-center', 'text-lg-center')}>
-                Mari Berdana
-              </h2>
-            </Col>
-            <Col md={6}>
-              <p className={clsx(Style.description)}>
-                Seluruh dana yang dikumpulkan akan digunakan untuk program kerja Vidyasena dan
-                operasional Vihara Vidyaloka
-              </p>
-              <Link href={'/dana'}>
-                <StyledButton size="sm" variant={'primary'}>
-                  Dana Sekarang
-                </StyledButton>
-              </Link>
-            </Col>
-          </Row>
-        </SectionNoTitle>
-      </div>
+      <Parallax
+        bgImage={DonateBannerImage.src}
+        bgImageAlt={'Donasi'}
+        style={{
+          width: '100%',
+        }}
+        contentClassName={Style.contentWrapper}
+        strength={400}
+        lazy
+      >
+        <div className="d-flex align-items-center justify-content-center h-100">
+          <SectionNoTitle>
+            <Row className={Style.content}>
+              <Col md={6} className="mx-md-auto mx-lg-auto my-md-auto my-lg-auto">
+                <h2 className={clsx(Style.title, 'text-md-center', 'text-lg-center')}>
+                  Mari Berdana
+                </h2>
+              </Col>
+              <Col md={6}>
+                <p className={clsx(Style.description)}>
+                  Seluruh dana yang dikumpulkan akan digunakan untuk program kerja Vidyasena dan
+                  operasional Vihara Vidyaloka
+                </p>
+                <Link href={'/dana'}>
+                  <StyledButton size="sm" variant={'primary'}>
+                    Dana Sekarang
+                  </StyledButton>
+                </Link>
+              </Col>
+            </Row>
+          </SectionNoTitle>
+        </div>
+      </Parallax>
     </div>
   );
 };
